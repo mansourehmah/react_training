a4de0922b57a34320ebd25d7cc7c985438c850f0
mansoureh mahzadeh <mansoureh.1998@gmail.com>
2021-03-17 17:55:49 +0330

    add files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Item from './components/item'
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <Item />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/css/index.css';
+import App from './App.jsx';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('app')
+);
+reportWebVitals();
